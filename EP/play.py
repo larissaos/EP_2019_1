76aedf56e9dab42cc04a3df9df0fbe47f09314c8
Kathleen Silva <kathleensn@al.insper.edu.br>
2019-04-22 13:43:08 -0300

    Aluno B esta feito

---
+++ EP/play.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 20:03:53 2019
+
+@author: User
+"""
+import json
+from os import system, name 
+# EP 2019-1: Escape Insper
+#
+# Alunos: 
+# - aluno A: Larissa Oliveira, larissaos@al.insper.edu.br
+# - aluno B: Kathleen da Silva Nascimento, kathleensn@al.insper.edu.br
+# - aluno C: Giovanna Alves Papandrea Neves, giovannaapn@al.insper.edu.br
+
+def carregar_cenarios():
+    with open('cenarios.json', encoding='utf8') as script:
+        cenarios = json.load(script)
+    nome_cenario_atual = "inicio"
+    return cenarios, nome_cenario_atual
+
+def clear(): 
+    #comando para limpar a tela, se for windows, se não, se for MAC ou Linux
+    if name == 'nt': 
+        _ = system('cls') 
+    else: 
+        _ = system('clear') 
+
+def main():
+    clear()
+    cenarios, nome_cenario_atual = carregar_cenarios()#aqui deve aparecer as opções, e conforme as ecolhas
+    primeiro_texto()
+    clear()
+    game_over = False #se você n morreu
+    while not game_over:# vc ta em algm lugar
+        cenario_atual = cenarios[nome_cenario_atual] 
+        
+        caracteres = "-"*len(cenarios[nome_cenario_atual]["título"])
+        
+        opcoes = cenario_atual['opções']
+        descricao = cenario_atual['descrição']
+        
+        print (cenario_atual["título"])
+        print (caracteres)
+        print(descricao)
+        print()
+        
+        
+        #if opcoes == andar professor:
+        #    escolha_andar_prof = input("Escolha o que você vai fazer: inicio ou professor"):
+        #        if escolha_andar_prof == "Tomar elevador para saguao"
+                
+        if len(opcoes) == 0:
+            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
+            game_over = True
+            print("Você morreu!")
+        else:
+            for opcao in cenario_atual["opções"]:
+                texto = cenario_atual["opções"][opcao]["texto"]
+                space = " "*(24-len(opcao))
+                print(opcao+":", end=space)
+                print(texto)
+            
+            print()
+            escolha = input("Escolha para onde você quer ir: ")
+            if escolha in opcoes:
+                clear()
+                nome_cenario_atual = opcoes[escolha]["cenario"]
+            else:
+                print("Sua indecisão foi sua ruína!")
+                game_over = True
+                print("Você morreu!")
+
+def primeiro_texto():
+    print("Na hora do sufoco!")#aqui se iniciam suas más escolhas
+    print("------------------")
+    print()
+    print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
+        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
+    print()#aqui vc resolve implorar por misericódia
+    print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
+        "na entrada do Insper, e quer procurar o professor para pedir um "
+        "adiamento do EP (boa sorte...)")
+    print()
+    print("Pressione Enter para continuar...")
+    input()
+    
+
+
+# Programa principal.
+if __name__ == "__main__":
+    main()
