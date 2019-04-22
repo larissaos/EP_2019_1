9cdd75f3d7116349210ffa3fb453befd1ea6f14b
giovannaapn <you@example.com>
2019-04-22 17:08:59 -0300

    Perguntando o nome 2.0

--- ep1.py
+++ ep1.py
@@ -72,6 +72,8 @@ def main():
                 
                 
 def primeiro_texto():
+    print("Como quer der chamado?")
+    name = input()
     print("Na hora do sufoco!")#aqui se iniciam suas más escolhas
     print("------------------")
     print()
